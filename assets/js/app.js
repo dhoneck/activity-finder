dfcd0ff50ebb1a4656c59d7279568f3fddab1fe7
Dustin Honeck <honeck_34@hotmail.com>
2023-01-12 21:07:46 -0600

    Add localstorage get and set functions for favorites

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,38 @@
-var fetchButton = document.getElementById('find-activity');
+var fetchBtn = document.getElementById('find-activity');
+var favoriteBtn = document.getElementById('favorite');
 var activityContainer = document.getElementById('activity-container');
 
 const GIPHY_API_KEY = 'YZ340zDN7GxGfiO0CaEmo7tnJkwSmxA1';
 
+function getFavorites() {
+  var favorites = window.localStorage.getItem('favorites');
+  if (favorites) {
+    return JSON.parse(favorites);
+  } else {
+    return [];
+  }
+}
 
-function getGiphyImg(activityDesc, activityType) { 
-  var encodedActivity = encodeURIComponent(activityDesc);
+function setFavorite(activity, gifURL) {
+  var favorites = getFavorites();
+  for (var x = 0; x < favorites.length; x++) {
+    if (favorites[x].activity === activity && favorites[x].gifURL === gifURL) {
+      console.log('"' + activity + '" is already in your list with that specific gif.');
+      return;
+    }
+  }
+  favorites.push(
+    {
+      "activity": activity,
+      "gifURL": gifURL,
+    }
+  );
+  window.localStorage.setItem('favorites', JSON.stringify(favorites));
+
+}
+
+function getGiphyImg(searchDesc, activityType) { 
+  var encodedActivity = encodeURIComponent(searchDesc);
   var requestUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&q=' + encodedActivity + '&lang=en';
 
   fetch(requestUrl)
@@ -15,10 +42,10 @@ function getGiphyImg(activityDesc, activityType) {
     .then(function (giphyData) {
       // Get gif data and create HTML elements
       var totalGifs = giphyData['data'].length;
-      var br = document.createElement('br');
       var img = document.createElement('img');
 
       if (totalGifs > 0) {
+        console.log('Found ' + totalGifs + ' results for: ' + searchDesc + ' | ' + activityType);
         // Select a gif
         var selectedGifIndex = Math.floor(Math.random() * totalGifs);
         var gif = giphyData['data'][selectedGifIndex];
@@ -26,12 +53,14 @@ function getGiphyImg(activityDesc, activityType) {
         // Set image attributes
         img.src = gif['images']['original']['url'];
         img.alt = gif['title'];
+        img.id = 'gif';
 
-        // Add elements to page
-        activityContainer.append(br);
+        // Add element to page
         activityContainer.append(img);
       } else {
-        // Select a gif based on type
+        // Send activity type as the new search description
+        console.log('Found 0 results for: ' + searchDesc + ' | ' + activityType);
+      
         getGiphyImg(activityType, activityType);
       }
       
@@ -59,11 +88,23 @@ function getApi(e) {
     .then(function (data) {
       var activityDesc = data['activity']; 
       var activityType = data['type']; 
-      activityContainer.textContent = activityDesc;
+      var p = document.createElement('p');
+
+      p.textContent = activityDesc;
+      p.id = 'activity';
+
+      activityContainer.innerHTML = '';
+      activityContainer.append(p);
 
       // Call Giphy API
       getGiphyImg(activityDesc, activityType);
     });
 }
 
-fetchButton.addEventListener('click', getApi);
+fetchBtn.addEventListener('click', getApi);
+favoriteBtn.addEventListener('click', function () {
+  var activity = document.getElementById('activity').textContent;
+  var gif = document.getElementById('gif').src;
+
+  setFavorite(activity, gif);
+})

26b7897f424238683ad1970f3fc9c4adec492960
Dustin Honeck <honeck_34@hotmail.com>
2023-01-17 20:31:04 -0600

    Display favorites in modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
+// Grab HTML Elements
 var fetchBtn = document.getElementById('find-activity');
 var favoriteBtn = document.getElementById('favorite');
 var activityContainer = document.getElementById('activity-container');
+var modalContainer = document.getElementById('favorites-modal');
+var viewFavorites = document.getElementById('view-favorites');
+var closeFavorites = document.getElementById('close-favorites');
+var modalBackground = document.getElementById('modal-background');
+var favoritesContainer = document.getElementById('favorites-container');
 
 const GIPHY_API_KEY = 'YZ340zDN7GxGfiO0CaEmo7tnJkwSmxA1';
 
+// Gets favorite activities from local storage
 function getFavorites() {
   var favorites = window.localStorage.getItem('favorites');
   if (favorites) {
@@ -13,6 +20,7 @@ function getFavorites() {
   }
 }
 
+// Sets an activity as a favorite in local storage
 function setFavorite(activity, gifURL) {
   var favorites = getFavorites();
   for (var x = 0; x < favorites.length; x++) {
@@ -29,9 +37,45 @@ function setFavorite(activity, gifURL) {
   );
   console.log('Adding "' + activity + '" to favorites');
   window.localStorage.setItem('favorites', JSON.stringify(favorites));
-
 }
 
+// Display favorites list to modal
+function displayFavorites() {
+  favoritesContainer.innerHTML = '';
+  var favorites = getFavorites();
+
+  var h2 = document.createElement('h2');
+  h2.textContent = 'Favorites';
+  h2.classList = 'is-size-2'
+  favoritesContainer.append(h2)
+
+  if (favorites.length > 0) {
+    for (var x = 0; x < favorites.length; x++) {
+      // Create elements
+      var div = document.createElement('div');
+      var h4 = document.createElement('h4');
+      var img = document.createElement('img');
+
+      // Add content and attributes to element
+      div.classList.add('my-5');
+      h4.textContent = favorites[x]['activity'];
+      h4.classList = 'is-size-4 mb-2';
+      img.src = favorites[x]['gifURL'];
+      img.alt = favorites[x]['activity'];
+
+      // Append objects
+      div.append(h4);
+      div.append(img);
+      
+      favoritesContainer.append(div);
+    }
+  } else {
+    var p  = document.createElement('p');
+    favoritesContainer.append(p);
+  }
+}
+
+// Get a gif from Giphy API based on activity description
 function getGiphyImg(searchDesc, activityType) { 
   var encodedActivity = encodeURIComponent(searchDesc);
   var requestUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&q=' + encodedActivity + '&lang=en';
@@ -68,7 +112,8 @@ function getGiphyImg(searchDesc, activityType) {
     });
 }
 
-function getApi(e) {
+// Get activity from Bored API
+function getActivity(e) {
   e.preventDefault();
   
   var requestUrl = 'https://www.boredapi.com/api/activity/';
@@ -102,13 +147,37 @@ function getApi(e) {
     });
 }
 
-fetchBtn.addEventListener('click', getApi);
-favoriteBtn.addEventListener('click', function () {
+// Event Listeners
+// Displays an activity and gif when button is clicked
+fetchBtn.addEventListener('click', getActivity);
+
+// Adds current activity and gif to favorites when clicked
+favoriteBtn.addEventListener('click', function (e) {
+  e.preventDefault();
   try {
     var activity = document.getElementById('activity').textContent;
     var gif = document.getElementById('gif').src;
     setFavorite(activity, gif);
   } catch {
-    console.log('No activity or gif to favorite.');
+    console.log('No activity or gif to favorite');
   }
+})
+
+// Shows favorite activities and gifs on click
+viewFavorites.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalContainer.classList.add('is-active');
+  displayFavorites();
+})
+
+// Closes favorites modal on modal button click
+closeFavorites.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalContainer.classList.remove('is-active');
+})
+
+// Closes favorites modal on modal background click
+modalBackground.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalContainer.classList.remove('is-active');
 })
